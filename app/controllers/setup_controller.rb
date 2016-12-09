84fcfcc5a318e39096bd15e0fac3c81a1e7cd959
Kevin Elliott <kevin@welikeinc.com>
2016-12-08 18:55:39 -0800

    The Setup workflow is beginning to take shape.

---
+++ app/controllers/setup_controller.rb
@@ -0,0 +1,13 @@
+class SetupController < ApplicationController
+  def first
+  end
+
+  def second
+  end
+
+  def third
+  end
+
+  def complete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'setup/first', as: :setup
+  post 'setup/second', as: :setup_second
+  post 'setup/third', as: :setup_third
+  post 'setup/complete', as: :setup_complete
+  get 'setup/hurray', as: :setup_hurray
+
   root to: 'visitors#index'
   devise_for :users
   resources :users
